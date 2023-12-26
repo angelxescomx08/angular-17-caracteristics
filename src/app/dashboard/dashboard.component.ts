9b3194716178013241ff1c790940a14526f6bc63
Jose Angel <jose.angel.hdz.rda@gmail.com>
2023-12-26 10:36:20 -0600

    rutas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: `./dashboard.component.html`,
   styles: `
     :host {
