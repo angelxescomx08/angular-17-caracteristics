674e5e9b92f1b58f2b9af572e71b8f6641bcb635
Jose Angel <jose.angel.hdz.rda@gmail.com>
2023-12-26 16:29:17 -0600

    Change detection

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,18 +1,35 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './change-detection.component.html',
-  styles: `
-    :host {
-      display: block;
-    }
-  `,
+  //para menos zonejs (que no este tan pendiente de los cambios)
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChangeDetectionComponent { }
+export class ChangeDetectionComponent {
+  public frameworkAsSignal = signal({
+    framework: 'Angular',
+    realise: '2023',
+  });
+
+  public frameworkAsProperty = {
+    framework: 'Angular',
+    realise: '2023',
+  };
+
+  constructor() {
+    setTimeout(() => {
+      //se dispara pero no se actualiza con el ChangeDetectionStrategy.OnPush
+      this.frameworkAsProperty.framework = 'React';
+
+      this.frameworkAsSignal.update((prev) => ({
+        ...prev,
+        framework: 'React',
+      }));
+    }, 2000);
+  }
+}
